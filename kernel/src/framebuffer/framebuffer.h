ff89e7648c92ae70c0af4fab364951356bfb99a4
Sable Ayala <sable262021@gmail.com>
2025-08-28 12:11:41 -0400

    at least it turns on

---
+++ kernel/src/framebuffer/framebuffer.h
@@ -0,0 +1,10 @@
+#include <limine.h>
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once or marked as used with the "used" attribute as done here.
+
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0};
